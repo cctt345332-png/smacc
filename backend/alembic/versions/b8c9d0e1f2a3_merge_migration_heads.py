438638ead6294669b7c562ae40b2d4dbad1e2f1d
cctt345332-png <2.34796624e+08+cctt345332-png@users.noreply.github.com>
2026-09-01 04:04:12 +0000

    Merge Alembic migration heads

---
+++ backend/alembic/versions/b8c9d0e1f2a3_merge_migration_heads.py
@@ -0,0 +1,18 @@
+"""merge accounting and reps migration heads
+
+Revision ID: b8c9d0e1f2a3
+Revises: a7b8c9d0e1f2, e3f4a5b6c7d8
+"""
+
+revision = "b8c9d0e1f2a3"
+down_revision = ("a7b8c9d0e1f2", "e3f4a5b6c7d8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
